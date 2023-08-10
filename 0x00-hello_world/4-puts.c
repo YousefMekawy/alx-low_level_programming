d5f972105bacf980a253e49f18c114689a5a9d1d
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-10 16:00:05 -0700

    dsd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main -retry point
+ * main - Entry point
  *
- *return always 0(succass)
+ * Return: always 0 (succass)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
