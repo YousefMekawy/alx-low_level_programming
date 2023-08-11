3e248b73e2b7fc3e26b3500345016bbb0967ad02
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-10 17:27:12 -0700

    ds

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  *
- * Retrun: Always 0 {Success)
+ * Retrun: Always 1 {Success)
  */
 int main(void)
 {
-	write(2 , "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , 59) ;
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
