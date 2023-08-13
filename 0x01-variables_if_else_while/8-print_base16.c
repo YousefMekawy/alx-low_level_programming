65c95f713049da3e684fda3c68ff307c599caeb8
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-13 08:35:24 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point fun for ABC
+ *
+ * Description: finction prints th alphapit
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	char hex[16] = "0123456789abcdef";
+	int i;
+
+	for (i = 0; i < 15; i++)
+		putchar(hex[i]);
+	putchar('\n');
+	return (0);
+}
