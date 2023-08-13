afe890fcc5a891d7da3b16da06766058ffeb0d27
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-13 06:31:18 -0700

    3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,14 @@
  */
 int main(void)
 {
-	char alphbit[26]="abcdefghijklmnopqrstuvwxyz"; 
+	char alphbit[26] = "abcdefghijklmnopqrstuvwxyz"
 	int i;
-	for(i=0;i <= 25;i++)
+
+	for (i = 0; i <= 25; i++)
 	{
+
 		putchar(alphbit[i]);
 	}
 	putchar('\n');
 	return (0);
-	
 }
