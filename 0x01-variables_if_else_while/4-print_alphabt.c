41b17645ecb26623d4fe59813643dd2d4205e8e2
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-13 07:23:04 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point fun for ABC
+ *
+ * Description: finction prints the alphapit exsept e&q
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+char alpabit[24] = "abcdfghijklmnoprstuvwxyz";
+        int i;
+
+        for (i = 0; i <= 23;  i++)
+                putchar(alpabit[i]);
+
+        putchar('\n');
+        return (0);
+}
