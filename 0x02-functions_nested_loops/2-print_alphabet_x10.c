ca9a4ca45037b841a6b03f4765bbdc20d67928c8
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-15 13:01:35 -0700

    wqe

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all aphabetfor ten times lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char b;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+	}
+}
