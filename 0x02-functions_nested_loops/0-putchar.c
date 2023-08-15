1a760dced62fa378ae9a5e84f3ee17b717c06be9
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-15 11:56:07 -0700

    header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point fun for print putchar
+ *
+ * Description: finction prints _putchar
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
