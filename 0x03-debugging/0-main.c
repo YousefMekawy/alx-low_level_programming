e65c4a8fbc7afad61258e45309649d0bbd0950d2
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-17 19:09:52 -0700

    weq

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* main - function that test if it pos or neg
-*
+* main - function that test if pov
 * Return: 0
 */
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
