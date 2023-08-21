11f0ee37773c5efe3e0af0e87ca44ceb45eb288c
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-21 16:23:09 -0700

    dsdsd

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,9 @@ void print_rev(char *s)
 	int i, temp, len;
 
 	len = _strlen(s);
-	for (i = 0; i < len / 2; i++)
+	for (i = len; i >= 0; i--)
 	{
-		temp[i] = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		_putchar(*s[i]);
 	}
+	_putcahr('\n');
 }
