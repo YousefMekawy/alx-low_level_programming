c8ab204bbc422e51d36b3642156a49efd015c138
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-21 16:55:29 -0700

    dsds

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints the sec half
+ * of a string
+ * @str: an input of string
+ */
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	for (i = (len / 2); i >= (len - 1); i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
