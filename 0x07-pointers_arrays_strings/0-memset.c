5a660f82c3cbecfe18fd14c701761ca8e700713f
YoussefMekawy <youssefmekawyof@gmail.com>
2023-08-28 15:03:08 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - a finction that sets a diffrent value
+ * in a string
+ * @s: the string that r given
+ * @b: the char that the user want to be replaced with
+ * @n: how many char that the user wantto change
+ * Return: pointer(s)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; n > 0; k++; n--;)
+		s[k] = b;
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+char *_strstr(char *haystack, char *needle);
+char *_memset(char *s, char b, unsigned int n);
